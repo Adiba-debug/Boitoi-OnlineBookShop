a533a40718add1aba51f2ea2776205b04df6f287
Moumita1 <moumitamou2005@gmail.com>
2026-09-20 19:11:26 +0600

    order status updating part

--- backend/hashPassword.js
+++ backend/hashPassword.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 
 async function hashPassword() {
-    const hashedPassword = await bcrypt.hash("1234", 10);
+    const hashedPassword = await bcrypt.hash("fairooz123", 10);
     console.log(hashedPassword);
 }
 
